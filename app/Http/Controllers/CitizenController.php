89ba414e829ab94e0f7b79f5b73982ae3151e21d
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-01-21 22:16:53 -0400

    feat: add citizens and application list all routes

--- app/Http/Controllers/CitizenController.php
+++ app/Http/Controllers/CitizenController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Citizen;
 use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
 
-class CitizenController extends Controller
+class CitizenController extends BaseController
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $citizens = Citizen::query();
+        
+        if($request->filled('cpf')){
+            $citizens->where('cpf', $request->cpf);
+        }
+
+        $citizens = $citizens->orderBy('name')->get();
+
+        return $this->sendResponse($citizens, 'Cidadãos recuperados com sucesso.');
     }
 
     /**
